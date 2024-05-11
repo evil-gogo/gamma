a3e7081cfb3ba33fa8a636c5047df8147784f3c0
Gagan Singh - g0s07jr <Gagan.Singh2@walmart.com>
2024-05-11 15:54:11 +0530

    hasta la vista baby

--- src/leetcode/p_394_decode_string/Solution.java
+++ src/leetcode/p_394_decode_string/Solution.java
@@ -1,34 +1,54 @@
 package leetcode.p_394_decode_string;
 
+//https://leetcode.com/problems/decode-string/description/
+
 import java.util.Stack;
 
 class Solution {
     public static String decodeString(String s) {
-        Stack<Character> stack = new Stack<>();
+        if (s.length() == 0 || s == null) {
+            return s;
+        }
+        Stack<String> stackString = new Stack<>();
+        Stack<Integer> stackNumber = new Stack<>();
 
-        StringBuilder tempSB = new StringBuilder();
-        StringBuilder poppedSB = new StringBuilder();
-        for (int i = 0; i < s.length(); i++) {
-            if (s.charAt(i) == Character.valueOf(']')) {
-                char c;
-                while ((stack.pop()) != Character.valueOf('[')) {
-                    poppedSB.append(stack.pop());
-                    System.out.println("stackI " + stack + stack.pop().equals("["));
+        StringBuilder sb = new StringBuilder();
+        int index = 0;
+        while (index < s.length()) {
+            if (Character.isDigit(s.charAt(index))) {
+                int num = 0;
+                while (Character.isDigit(s.charAt(index))) {
+                    num = num * 10 + (s.charAt(index) - '0');
+                    index++;
                 }
+                stackNumber.push(num);
             } else {
-                stack.push(s.charAt(i));
-                System.out.println("stackE " + stack);
+                switch (s.charAt((index))) {
+                    case '[':
+                        stackString.push(sb.toString());
+                        sb = new StringBuilder();
+                        index++;
+                        break;
+                    case ']':
+                        StringBuilder tempSb = new StringBuilder(stackString.pop());
+                        int repeatTimes = stackNumber.pop();
+                        for (int i = 0; i < repeatTimes; i++) {
+                            tempSb.append(sb);
+                        }
+                        sb = tempSb;
+                        index++;
+                        break;
+                    default:
+                        sb.append(s.charAt(index++));
+                }
             }
         }
-        System.out.println("stack " + stack);
-        System.out.println("popped SB " + poppedSB);
-        return null;
-
+        return sb.toString();
     }
 
     public static void main(String[] args) {
-        //String s = "3[a]2[bc]";
-        String s = "3[a2[c]]";
+        String s = "3[a]2[bc]";
+        //String s = "3[a2[c]]";
         System.out.println(decodeString(s));
     }
 }

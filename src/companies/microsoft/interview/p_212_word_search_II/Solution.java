cbc5c51512823f922b6a746b0031fe099ae882ed
Gagan Singh <gagan@MacBook-Air.local>
2024-06-13 02:15:37 +0530

    dsd

---
+++ src/companies/microsoft/interview/p_212_word_search_II/Solution.java
@@ -0,0 +1,89 @@
+package companies.microsoft.interview.p_212_word_search_II;
+
+//https://leetcode.com/problems/word-search-ii/description/
+
+import java.util.ArrayList;
+import java.util.List;
+
+class TrieNode {
+    TrieNode[] children;
+    boolean isEndOfWord;
+    int referenceWordIndex;
+
+    TrieNode() {
+        children = new TrieNode[26];
+        isEndOfWord = false;
+    }
+}
+
+class Trie {
+    TrieNode root;
+
+    public Trie() {
+        root = new TrieNode();
+    }
+
+    public void insert(String word, int referenceWordIndex) {
+        TrieNode temp = root;
+        for (int i = 0; i < word.length(); i++) {
+            int index = word.charAt(i) - 'a';
+            if (temp.children[index] == null) {
+                temp.children[index] = new TrieNode();
+            }
+            temp = temp.children[index];
+        }
+        temp.isEndOfWord = true;
+        temp.referenceWordIndex = referenceWordIndex;
+    }
+}
+
+class Solution {
+    public static List<String> findWords(char[][] board, String[] words) {
+        Trie trie = new Trie();
+        for (int i = 0; i < words.length; i++) {
+            trie.insert(words[i], i);
+        }
+
+        List<String> result = new ArrayList<>();
+        for (int i = 0; i < board.length; i++) {
+            for (int j = 0; j < board[0].length; j++) {
+                dfs(board, words, i, j, trie.root, result);
+            }
+        }
+        return result;
+    }
+
+    private static void dfs(char[][] board, String[] words, int i, int j, TrieNode trieNode, List<String> result) {
+        int charIndex = board[i][j] - 'a';
+
+        if (trieNode.children[charIndex] == null) {
+            return;
+        }
+
+        trieNode = trieNode.children[charIndex];
+
+        if (trieNode.isEndOfWord && trieNode.referenceWordIndex != -1) {
+            result.add(words[trieNode.referenceWordIndex]);
+            trieNode.referenceWordIndex = -1;
+        }
+
+        char tempChar = board[i][j];
+        int[] directions = {-1, 0, 1, 0, -1};
+
+        board[i][j] = '#';
+        for (int k = 0; k < 4; k++) {
+            int newRow = i + directions[k];
+            int newCol = j + directions[k + 1];
+            if (newRow >= 0 && newRow < board.length && newCol >= 0 && newCol < board[0].length && board[newRow][newCol] != '#') {
+                dfs(board, words, newRow, newCol, trieNode, result);
+            }
+        }
+        board[i][j] = tempChar;
+    }
+
+    public static void main(String[] args) {
+        char[][] board = {{'o', 'a', 'a', 'n'}, {'e', 't', 'a', 'e'}, {'i', 'h', 'k', 'r'}, {'i', 'f', 'l', 'v'}};
+        String[] words = {"oath", "pea", "eat", "rain"};
+        System.out.println(findWords(board, words));
+    }
+}
---
+++ src/leetcode/p_329_longest_increasing_path_in_a_matrix/Solution.java
@@ -0,0 +1,39 @@
+package leetcode.p_329_longest_increasing_path_in_a_matrix;
+
+//https://leetcode.com/problems/longest-increasing-path-in-a-matrix/description/
+
+class Solution {
+    public static int longestIncreasingPath(int[][] matrix) {
+        int longestPath = 0;
+        int[][] dp = new int[matrix.length][matrix[0].length];
+        for (int i = 0; i < matrix.length; i++) {
+            for (int j = 0; j < matrix[0].length; j++) {
+                longestPath = Math.max(longestPath, dfs(matrix, i, j, dp));
+            }
+        }
+        return longestPath;
+    }
+
+    private static int dfs(int[][] matrix, int row, int col, int[][] dp) {
+        if (dp[row][col] != 0) {
+            return dp[row][col];
+        }
+
+        int[] directions = {-1, 0, 1, 0, -1};
+
+        for (int k = 0; k < 4; k++) {
+            int nextRow = row + directions[k];
+            int nextCol = col + directions[k + 1];
+
+            if (nextRow >= 0 && nextRow < matrix.length && nextCol >= 0 && nextCol < matrix[0].length && matrix[nextRow][nextCol] > matrix[row][col]) {
+                dp[row][col] = Math.max(dp[row][col], dfs(matrix, nextRow, nextCol, dp));
+            }
+        }
+        return dp[row][col] = 1 + dp[row][col];
+    }
+
+    public static void main(String[] args) {
+        int[][] matrix = {{9, 9, 4}, {6, 6, 8}, {2, 1, 1}};
+        System.out.println(longestIncreasingPath(matrix));
+    }
+}
